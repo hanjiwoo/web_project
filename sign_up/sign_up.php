009102874e90e5e17d9f0b61f3a9580effa05d8f
hanjiwoo <jiw021538@gmail.com>
2018-09-12 20:10:33 +0900

    overlap_c.php modify/ sign_up.js new/ dbconnect.php new/ index.php modify/ sign_up.php modify/ sign_up.css modify

--- sign_up/sign_up.php
+++ sign_up/sign_up.php
@@ -1,9 +1,6 @@
 <?php
-    $host='localhost';
-    $user='root';
-    $user_pwd=2464;
-    $dbName='web_test';
-    
+    include "./dbconnect.php";
+
     $name=$_POST['name'];
     $id=$_POST['id'];
     $pwd=$_POST['pwd'];
@@ -11,20 +8,11 @@
     $nick=$_POST['nick'];
     $birth=$_POST['birth'];
     
-    // if($pwd!=$pwd_c)
-    // {
-    //     echo "password confirm is different from password";
-    //     echo "<a href=./main.php>back page</a>";
-    //     exit();
-    // }
-    
-    $mysqli=new mysqli("$host","$user",$user_pwd,"$dbName");
-
     $signup=mysqli_query($mysqli,"INSERT INTO user_info (user_name,user_id,user_pwd,user_nick,user_birth)
     VALUES('$name','$id','$pwd','$nick','$birth')");
     if($signup)
     {
-        echo "sign up success";
+        echo("<script>location.replace('./index.php');</script>");
     }
     else
     {
